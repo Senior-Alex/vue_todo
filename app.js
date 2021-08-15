baa9dcd44512752321a7c08d515f9cf37e2dc4ae
Senior-Alex <59695985+Senior-Alex@users.noreply.github.com>
2021-08-15 22:32:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const App = {
+  data() {
+    return {
+      placeholderString: 'Введите название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+      notes: []
+    }
+  },
+  methods: {
+    addNewNote() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    deleteNote(idx) {
+      this.notes.splice(idx, 1)
+    }
+  }
+}
+
+Vue.createApp(App).mount('#app')
